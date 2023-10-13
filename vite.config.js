1ae04613e57c5f888639f5c4b9b606b4a7e914cb
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2023-10-13 10:06:09 +0100

    Resolving repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-tic-tac-toe/"
+  base: "/ReactJS-tic-tac-toe/"
 })
